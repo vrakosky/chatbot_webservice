fb26fb253f0e54c55c09e3584c9cc8f9462fb5e3
vrakosky <vrakosky.083@gmail.com>
2018-12-12 09:17:49 +0100

    [server]Adding new element 'User' in ChatControlleur.java and Message.java

--- server/src/main/java/hello/ChatController.java
+++ server/src/main/java/hello/ChatController.java
@@ -8,6 +8,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 import static org.springframework.http.MediaType.*;
 
 
+import org.json.JSONObject;
 @RestController
 public class ChatController {
 
@@ -18,11 +19,24 @@ public class ChatController {
     @RequestMapping(value="/api/new-message",
                     method = RequestMethod.POST,
                     consumes = TEXT_PLAIN_VALUE)
-    public void newMessage(@RequestBody String text) {
-        Message m = new Message(text);
+    public void newMessage(@RequestBody String fullMessage) {
+        JSONObject fullMessageParsed = new JSONObject(fullMessage);
+        Message m = new Message(fullMessageParsed.getString("txt"),fullMessageParsed.getString("user"));
         System.out.println(m.getText());
+        System.out.println(m.getUser());
+
         messages.add(m);
+        //chatBotReponse(text);
     }
+
+    //Simple chatbot
+    void chatBotReponse (String text, String user) {
+        if (text.equals("meteo")) {
+            Message reponse = new Message("Il pleut : "+text, "Robot");
+            messages.add(reponse);
+        }
+    }
+
 
     // Our client is not on the same server, so we need to
     @CrossOrigin // allow request from all origins
@@ -31,10 +45,11 @@ public class ChatController {
     //                             method = RequestMethod.GET)
     public Queue<Message> messages() {
         return messages;
+    }
+
 
     @GetMapping("/testing/")   
     public String random() {
         return "test";
-    }
     }
 }
--- server/src/main/java/hello/Message.java
+++ server/src/main/java/hello/Message.java
@@ -5,15 +5,18 @@ import java.util.Date;
 public class Message {
 
     // once assigned, never changes
-    private final Date date;
     private final String txt;
+    private final Date date;
+    private final String user;
 
     /**
        Creates a message using a given String and the current date
     **/
-    public Message(String message) {
+    public Message(String message, String user) {
         this.date = new Date();
         this.txt = message;
+        this.user = user;
+
     }
         
     public Date getDate() {
@@ -22,5 +25,9 @@ public class Message {
 
     public String getText() {
         return txt;
+    }
+
+    public String getUser() {
+        return user;
     }
 }
